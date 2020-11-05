a0481de492c7ec5ebbe8954b3da18bad5bdbbe94
Thom Wright <dev@thomwright.co.uk>
2020-11-05 21:38:12 +0000

    Keep memory of which file was selected
    Scenario:
    
    - open app, have dir selected in left
    - move right, move down 2
    - move left
    - move right
    
    Now, when moving right, we again select the file 'down 2', rather than
    the first in the list.

--- src/app.rs
+++ src/app.rs
@@ -7,20 +7,30 @@ struct FileList {
     selected_file: Option<usize>,
 }
 impl FileList {
+    fn new(files: Vec<FileInfo>) -> FileList {
+        let selected_file = if files.len() > 0 { Some(0) } else { None };
+        FileList {
+            files,
+            selected_file,
+        }
+    }
+
+    fn new_with_path_selected(
+        files: Vec<FileInfo>,
+        path: &PathBuf,
+    ) -> FileList {
+        let selected_file =
+            files.binary_search_by_key(path, |f| f.path.clone());
+        FileList {
+            files,
+            selected_file: selected_file.ok(),
+        }
+    }
+
     fn get_selected_file(&self) -> Option<&FileInfo> {
         match self.selected_file {
             None => None,
             Some(i) => self.files.get(i),
-        }
-    }
-
-    fn unselect_file(&mut self) {
-        self.selected_file = None
-    }
-
-    fn select_first(&mut self) {
-        if self.files.len() > 0 {
-            self.selected_file = Some(0);
         }
     }
 
@@ -51,6 +61,7 @@ impl FileList {
         }
     }
 }
+
 impl Default for FileList {
     fn default() -> Self {
         FileList {
@@ -101,15 +112,8 @@ impl State {
     pub fn new() -> Result<State, std::io::Error> {
         let init_path = std::env::current_dir()?;
         let current_dir_files = State::read_file_list(&init_path);
-        let selected_file = current_dir_files.first().map(|_| 0);
 
-        let flists = [
-            FileList {
-                files: current_dir_files,
-                selected_file,
-            },
-            FileList::default(),
-        ];
+        let flists = [FileList::new(current_dir_files), FileList::default()];
 
         let mut state = State {
             base_path: init_path,
@@ -117,10 +121,7 @@ impl State {
             selected_flist: 0,
         };
 
-        state.flists[1] = FileList {
-            files: state.files_in_selected_dir(),
-            selected_file: None,
-        };
+        state.flists[1] = FileList::new(state.files_in_selected_dir());
 
         Ok(state)
     }
@@ -143,22 +144,15 @@ impl State {
 
     pub fn on_left(&mut self) {
         if self.selected_flist != 0 {
-            self.current_list_mut().unselect_file();
             self.selected_flist -= 1;
         } else if let Some(parent) = self.base_path.parent() {
             let parent = parent.to_owned();
 
-            self.current_list_mut().unselect_file();
-
-            let files = State::read_file_list(&parent);
-            let selected_file =
-                files.binary_search_by_key(&self.base_path, |f| f.path.clone());
             self.flists = [
-                FileList {
-                    files,
-                    // FIXME: I bet we can somehow go into a parent directory with no visible files...
-                    selected_file: selected_file.ok(),
-                },
+                FileList::new_with_path_selected(
+                    State::read_file_list(&parent),
+                    &self.base_path,
+                ),
                 self.flists[0].clone(),
             ];
 
@@ -170,19 +164,11 @@ impl State {
         if self.selected_file_is_dir() {
             if self.space_to_right() {
                 self.selected_flist += 1;
-                self.current_list_mut().select_first();
             } else if let Some(selected_dir_path) = self.selected_file_path() {
                 let selected_dir_path = selected_dir_path.clone();
 
                 let files = State::read_file_list(&selected_dir_path);
-                let selected_file = files.first().map(|_| 0);
-                self.flists = [
-                    self.flists[1].clone(),
-                    FileList {
-                        files,
-                        selected_file,
-                    },
-                ];
+                self.flists = [self.flists[1].clone(), FileList::new(files)];
 
                 // FIXME: no unwrap!
                 self.base_path = selected_dir_path.parent().unwrap().to_owned();
@@ -191,7 +177,13 @@ impl State {
     }
 
     pub fn selected_file_in_list(&self, list: usize) -> Option<usize> {
-        self.flists.get(list).and_then(|l| l.selected_file)
+        // We don't count selections below where we are in the directory tree,
+        // they're simply kept as a 'memory' for when we re-select that list.
+        if list <= self.selected_flist {
+            self.flists.get(list).and_then(|l| l.selected_file)
+        } else {
+            None
+        }
     }
 
     pub fn files_in_list(&self, list: usize) -> Option<&Vec<FileInfo>> {
@@ -228,10 +220,8 @@ impl State {
 
     fn refresh_files_to_right(&mut self) {
         if self.space_to_right() {
-            self.flists[self.selected_flist + 1] = FileList {
-                files: self.files_in_selected_dir(),
-                selected_file: None,
-            };
+            self.flists[self.selected_flist + 1] =
+                FileList::new(self.files_in_selected_dir());
         }
     }
 
